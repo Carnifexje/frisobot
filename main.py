2f7a97468dc50e00606c5e5a4333132541d0010c
William van den Bosch <Carnifexje@users.noreply.github.com>
2021-08-02 22:18:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import http
-
+import requests
+import logging
 from flask import Flask, request
 from werkzeug.wrappers import Response
 
@@ -8,15 +9,53 @@ from telegram import Bot, Update
 from telegram.ext import Dispatcher, Filters, MessageHandler, CallbackContext
 
 app = Flask(__name__)
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+chat_id = os.environ["CHAT_ID"]
 
 
-def echo(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(update.message.text)
+def check(update: Update, context: CallbackContext) -> None:
+    chargers_to_check = [
+        'https://oplaadpalen.nl/wms?REQUEST=GetFeatureInfo&SERVICE=WMS&SRS=EPSG%3A4326&VERSION=1.1.1&INFO_FORMAT=application%2Fjson&BBOX=4.435096979141236%2C52.12392140665582%2C4.453373551368714%2C52.126305847735765&HEIGHT=724&WIDTH=3407&LAYERS=eco%3Arta_and_clusters&QUERY_LAYERS=eco%3Arta_and_clusters&X=1483&Y=220',
+        'https://oplaadpalen.nl/wms?REQUEST=GetFeatureInfo&SERVICE=WMS&SRS=EPSG%3A4326&VERSION=1.1.1&INFO_FORMAT=application%2Fjson&BBOX=4.426814317703248%2C52.12497246674536%2C4.463367462158204%2C52.12974110884798&HEIGHT=724&WIDTH=3407&LAYERS=eco%3Arta_and_clusters&QUERY_LAYERS=eco%3Arta_and_clusters&X=1520&Y=294'
+    ]
+
+    for charger in chargers_to_check:
+        r = requests.get(charger)
+        logger.info(r.text)
+        if r.status_code != requests.codes.ok:
+            logger.warning('Something went wrong getting the charger location: %s', r.status_code)
+            context.bot.send_message(chat_id=chat_id, text="I could not retrieve the location of the charger. Someone needs to look into this!")
+            continue
+        charger_ref = r.json()['features'][0]['properties']['external_reference']
+        r = requests.get(f'https://oplaadpalen.nl/api/map/location/{charger_ref}')
+        logger.info(r.text)
+        if r.status_code != requests.codes.ok:
+            logger.warning('Something went wrong getting the charger info: %s', r.status_code)
+            context.bot.send_message(chat_id=chat_id, text="I could not retrieve the status information of the charger. Someone needs to look into this!")
+            continue
+        data = r.json()['data']
+        address = data['address']
+        sockets = data['evses']
+        tariffs = sockets[0]['connectors'][0]['tariffs'][0]
+    
+        tariff = tariffs['price'] + (tariffs['price'] * (tariffs['vat'] / 100))
+        currency = tariffs['currency']
+
+        num_available = 0
+        for s in sockets:
+            num_available += (1 if s['status'] == 'AVAILABLE' else 0)
+               
+        context.bot.send_message(chat_id=chat_id, text=f'{num_available} socket(s) available at {address} for {tariff} {currency} / KWh')
+
 
 bot = Bot(token=os.environ["TOKEN"])
 
 dispatcher = Dispatcher(bot=bot, update_queue=None, workers=0)
-dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, check))
 
 @app.route("/", methods=["POST"])
 def index() -> Response:
