7e742898c0c512295932c587afc331cebf498802
William van den Bosch <Carnifexje@users.noreply.github.com>
2023-03-10 12:40:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -19,12 +19,21 @@ chat_id = os.environ["CHAT_ID"]
 
 def check(update: Update, context: CallbackContext) -> None:
     chargers_to_check = [
-        '4a11eb9dc242010a840003f992a358ef',
-        'eb11ed8d8a42010aa40fc000f02ba239',
-        'c011ea858042010a840021dde32062d7'
+        'https://oplaadpalen.nl/wms?REQUEST=GetFeatureInfo&SERVICE=WMS&SRS=EPSG%3A4326&VERSION=1.1.1&INFO_FORMAT=application%2Fjson&BBOX=4.435096979141236%2C52.12392140665582%2C4.453373551368714%2C52.126305847735765&HEIGHT=724&WIDTH=3407&LAYERS=eco%3Arta_and_clusters&QUERY_LAYERS=eco%3Arta_and_clusters&X=1483&Y=220',
+        'https://oplaadpalen.nl/wms?REQUEST=GetFeatureInfo&SERVICE=WMS&SRS=EPSG%3A4326&VERSION=1.1.1&INFO_FORMAT=application%2Fjson&BBOX=4.426814317703248%2C52.12497246674536%2C4.463367462158204%2C52.12974110884798&HEIGHT=724&WIDTH=3407&LAYERS=eco%3Arta_and_clusters&QUERY_LAYERS=eco%3Arta_and_clusters&X=1520&Y=294',
+        'https://oplaadpalen.nl/wms?REQUEST=GetFeatureInfo&SERVICE=WMS&SRS=EPSG%3A4326&VERSION=1.1.1&INFO_FORMAT=application%2Fjson&BBOX=4.43524718284607%2C52.12657251589268%2C4.4535934925079355%2C52.12903585048001&HEIGHT=748&WIDTH=3420&LAYERS=eco%3Arta_and_clusters&QUERY_LAYERS=eco%3Arta_and_clusters&X=1597&Y=317'
     ]
 
-    for charger_ref in chargers_to_check:
+    for charger in chargers_to_check:
+        r = requests.get(charger)
+        logger.info(r.text)
+        charger_location_json = r.json()
+        if r.status_code != requests.codes.ok || charger_location_json['features'].len() == 0:
+            logger.warning('Something went wrong getting the charger location: %s', charger)
+            context.bot.send_message(chat_id=chat_id, text="I could not retrieve the location of a charger on the map. Someone needs to look into this!")
+            continue
+        charger_ref = charger_location_json['features'][0]['properties']['external_reference']
+        
         r = requests.get(f'https://oplaadpalen.nl/api/map/location/{charger_ref}')
         logger.info(r.text)
         json_response = r.json()
